59f39d41dbd4bacd3f08f39d5b59a88962adc99a
Saddam Omar <52155202+drschimek@users.noreply.github.com>
2023-05-11 16:26:43 +0300

    Update C++_Multiplication number.cpp

--- Programming C++/C++_Multiplication number.cpp
+++ Programming C++/C++_Multiplication number.cpp
@@ -6,27 +6,15 @@ int main() {
 
   int num1, num2, result;
 
-  
-
   cout << "Enter first number: ";
-
   cin >> num1;
 
-  
-
   cout << "Enter second number: ";
-
   cin >> num2;
-
-  
 
   result = num1 * num2;
 
-  
-
   cout << "Result: " << result << endl;
-
-  
 
   return 0;
 
