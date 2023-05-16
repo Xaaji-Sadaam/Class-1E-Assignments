c8b2452bbf2b65cbba73fb7800f5781e76897de9
Saddam Omar <52155202+drschimek@users.noreply.github.com>
2023-05-16 13:52:20 +0300

    Create Output on screen.cpp

---
+++ Programming C++/Output on screen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "9" << endl;
+    cout << "18" << endl;
+    cout << "128" << endl;
+    cout << "*********************" << endl;
+    cout << "A\tB\tC" << endl;
+    cout << "\tD" << endl;
+    cout << "E\tF\tG" << endl;
+
+    return 0;
+}
