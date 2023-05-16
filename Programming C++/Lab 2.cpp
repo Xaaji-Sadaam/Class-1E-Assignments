cf977bcbaccd1ed37f7ae48491305cee4d26f059
Saddam Omar <52155202+drschimek@users.noreply.github.com>
2023-05-16 13:53:24 +0300

    Create Lab 2.cpp

---
+++ Programming C++/Lab 2.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+int main() {
+  string name = "Saddam Omar";
+  string college = "Hargeisa University";
+  string city = "Hargeisa";
+
+  cout << "My name is " << name << " and I did B.S.C. from " << college << " in " << city << "." << endl;
+
+  return 0;
+}
