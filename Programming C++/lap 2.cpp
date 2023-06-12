a63eba12294ec9890780ef3ed114d9bc98d6d275
Saddam Omar <52155202+drschimek@users.noreply.github.com>
2023-06-12 09:13:23 -0400

    Create lap 2.cpp

---
+++ Programming C++/lap 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "       9\n";
+    std::cout << "       18\n";
+    std::cout << "       128\n";
+    std::cout << "***********\n";
+    std::cout << "A       B       C\n";
+    std::cout << "        D\n";
+    std::cout << "E       F       G\n";
+
+    return 0;
+}
