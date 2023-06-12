54f205a63cfb16eef0b326debd419951edcc4c19
Saddam Omar <52155202+drschimek@users.noreply.github.com>
2023-06-12 09:16:04 -0400

    Create Lap 2_part2.cpp

---
+++ Programming C++/Lap 2_part2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "*****************************************\n";
+    std::cout << "*Programming Assignment 1*\n";
+    std::cout << "*Computer Programming 1*\n";
+    std::cout << "*Author: Saddam Omar ALi*\n";
+    std::cout << "*Due Date: Thursday 06 2023*\n";
+    std::cout << "* ***********************************************\n";
+
+    return 0;
+}
